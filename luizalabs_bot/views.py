f59d6fba6b97edd01e246932ece2a6cf2a4acee0
Paulo Batista <paulobatista.phd@gmail.com>
2017-10-31 18:55:08 +0000

    adding bootstrap

--- luizalabs_bot/views.py
+++ luizalabs_bot/views.py
@@ -3,44 +3,31 @@ from django.views import generic
 from django.http.response import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from googleapiclient.discovery import build
 import json, requests, random, re
 from pprint import pprint
 
+
+#Definicao dos tokens
 PAGE_ACCESS_TOKEN = "EAACBZAi171kMBACRtwqIKYVGbDoPR14RaXLh4fClBqSdqyCnSgaDVlLIMOxuZB0p8jO9dCJNWp2fHTCnM9HsIZAFNyqjuPNVppFi01qUPQtKUHrmb0ZCJlvJzq56k1QeUTmlbpty5fYuzOCDwrCnjZAZAURxVHQGbNpZCmwgT8BMqrxLl0LfvpO"
 VERIFY_TOKEN = "EAACBZAi171kMBAEgg9wxfrhZBjv4qCawMwEyEA4dUcH8nCk7dYHtC4GpFjAHTlvJlaP3ZCV11ZCTvZBzjsfowe1VLTm2SUnkKpAyhcblWHsBMRbicYImcbRQMPmTvZC96w7LXZBg1ZAchzbk1cDpS9y9SH3zVeRBydoQipVZA16N89V3Bxf8dUhxh"
 
+
+# Dicionario de dados, usando apenas 3 tokens de exemplos, bem longe da realidade.
+# poderia ser implementado usando um db, webservices ou pages requests
 luiza_mind = { 'celular': ["""Encontrei excelentes ofertas sobre Brinquedos para voce: http://www.magazineluiza.com.br/celulares-e-smartphones/l/te/""", 
                            """iPhone 8 - PRE-VENDA !!!!!, Confira: http://www.magazineluiza.com.br/landingpage/?header=231017PreVendaiPhone8header.png&unavailable=false&bob=true&menu=selecao-21939&showcase=selecao-21939"""], 
                'tv':      ["""Encontrei excelentes ofertas em TV's para voce: http://www.magazineluiza.com.br/tv-led-plasma-lcd-e-outras/tv-e-video/s/et/peco/ """, 
                           """ Conheca a LG 4k: http://www.magazineluiza.com.br/tv-led-plasma-lcd-e-outras/tv-e-video/s/et/peco/"""], 
-               'brinquedos':  ["""Encontrei excelentes ofertas sobre Brinquedos para voce: http://www.magazineluiza.com.br/brinquedos/l/br/ """, 
+               'brinquedo':  ["""Encontrei excelentes ofertas sobre Brinquedos para voce: http://www.magazineluiza.com.br/brinquedos/l/br/ """, 
                            """Confira ofertas especiais em Brinquedos: http://www.magazineluiza.com.br/brinquedos/l/br/"""] }
                            
-# Google API access key and custom search ID
-my_api_key = "AIzaSyDq75XYyUe525_k0C3kP-8Za0z_kK5Ak1s"
-my_cse_id = "015323979992596059899:s5hyzuypkl8magazineluiza"
-
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(
-        q=search_term, 
-        cx=cse_id, 
-        num=10
-        ).execute()
-    return res
-    
-# This function should be outside the BotsView class
+#  Metodo que envia dados para o Messenger em resposta a iteracao com o usuario
 def post_facebook_message(fbid, user_message):  
     
-    # Remove all punctuations, lower case the text and split it based on space
+    # tokenization 
     tokens = re.sub(r"[^a-zA-Z0-9\s]",' ', user_message).lower().split()
-    luiza_talks = "Ola ! Sou a Luiza, tenho 3 grandes promocoes para voce,  por favor, escolha qual promocao voce teria interesse, TV,  celular ou brinquedos!"
-    #luiza_talks = "Ola ! Sou a Luiza, tenho 3 grandes promocoes para voce,  por favor, escolha qual promocao voce teria interesse, TV,  celular ou Home Theater!"
-    #if welcome_message == True:
-    #    luiza_talks = "Ola ! Sou a Luiza, tenho 3 grandes promocoes para voce,  por favor, escolha qual promocao voce teria interesse, TV,  celular ou Home Theater!"
-    #    welcome = False    
-    #else:
+    luiza_talks = "Ola ! Sou a Luiza, tenho 3 grandes promocoes para voce,  por favor, escolha qual promocao voce teria interesse, digite frases que contenha uma palavra chave como TV,  celular ou brinquedos."
+   
     for token in tokens:
         if token in luiza_mind:
             luiza_talks = random.choice(luiza_mind[token])
@@ -49,36 +36,32 @@ def post_facebook_message(fbid, user_message):
     if not luiza_talks:
             luiza_talks = "Sua consulta nao foi possivel, por favor, informe um produto que gostaria de consultar" 
     
-    
-    
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
-    
-    #user_details_params = {'url':user_details_url, 'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
     user_details_params = {'fields':'first_name', 'access_token':PAGE_ACCESS_TOKEN}
-    #requests.get(user_details_url, user_details_params).json() 
     resp = requests.get(url=user_details_url, params=user_details_params)
     data = json.loads(resp.text)
     luiza_talks = data['first_name']+', ' + luiza_talks
-   
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":luiza_talks}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    
+    #somente para depuracao pela console, nao tem efeito pratico na app
     pprint(status.json())
     
+#Classe da view principal    
 class ChatBotView(generic.View):
     
+    # Metodos de iteracao
     def get(self, request, *args, **kwargs):
         if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')  
             
-    # The get method is the same as before.. omitted here for brevity
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
         
-    # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
         # Converts the text payload into a python dictionary
         incoming_message = json.loads(self.request.body.decode('utf-8'))
